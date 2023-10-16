006f8e906ff7fdaa0605675f161e5d6a3671c80d
Anand <ak7610052@gmail.com>
2023-10-16 10:46:43 +0530

    nothing changed

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,15 @@ const express = require('express');
 const app = express();
 const port = 8085;
 app.get('/',(req,res) =>{
-    res.send("hello anand")
+    res.send("hello Anand")
+})
+
+app.get('/login', (req, res) => {
+    res.send("hello this is the login page")
+})
+
+app.get('/signup', (req, res) => {
+    res.send("hello this is the signup page")
 })
 
 app.listen(port, () =>{
